5f0131c112e1dd2565df6cd6b9c0360c6074f7fd
Aivenest <aivenest@gmail.com>
2022-09-15 21:59:41 +0500

    Example005ElseIf

---
+++ Example005ElseIf/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Как Вас зовут?");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") 
+    {
+        Console.WriteLine("Привет, Машенька!");
+    }
+else
+    {
+        Console.Write("Привет, ");
+        Console.Write(username);
+    }
