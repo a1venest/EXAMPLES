95b44ff760fd10a3a5b1778035f7b321d7365cb9
Aivenest <aivenest@gmail.com>
2022-09-19 19:12:48 +0500

    Example011 compleated

--- Example011ArrayLibrary/Program.cs
+++ Example011ArrayLibrary/Program.cs
@@ -24,12 +24,13 @@ int IndexOf(int[] collection, int find)
 {
     int count = collection.Length;
     int index = 0;
-    int position = 0;
+    int position = -1;
     while(index<count)
     {
         if(collection[index]==find)
         {
             position=index;
+            break;
         }
         index++;
     }
@@ -42,5 +43,5 @@ FillArray(array);
 PrintArray(array);
 Console.WriteLine();
 
-int pos = IndexOf(array,4);
+int pos = IndexOf(array,444);
 Console.WriteLine(pos);
