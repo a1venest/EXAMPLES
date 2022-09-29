df5cd8d4a3d43b2b51ef6aae49d8455f675c7332
Aivenest <aivenest@gmail.com>
2022-09-29 10:07:18 +0500

    newText01

---
+++ Example012Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+/* Работа с текстом
+дан текст, в тексте нужно все пробелы заменить черточками,
+маленькие буквы "к" заменить большими "К",
+а большие "С" заменить маленькими "с".*/
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Выдадите мне чаю?";
+
+string Replase(string text, char oldValue,char newValue)
+{
+    string result = string.Empty;
+
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if(text[i]==oldValue) result = result +$"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+string newText = Replase(text, ' ', '|');
+Console.WriteLine(newText);
