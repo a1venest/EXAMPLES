39adfa10bc01a9cbab3fa72642d622395f676b5c
Aivenest <aivenest@gmail.com>
2022-09-29 11:34:27 +0500

    Home Work

--- Example012Methods/Program.cs
+++ Example012Methods/Program.cs
@@ -12,29 +12,98 @@
 маленькие буквы "к" заменить большими "К",
 а большие "С" заменить маленькими "с".*/
 
-string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
-            + "ежели бы вас послали вместо нашего милого Винценгероде,"
-            + "вы бы взяли приступом согласие прусского короля. "
-            + "Вы так красноречивы. Выдадите мне чаю?";
+// string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+//             + "ежели бы вас послали вместо нашего милого Винценгероде,"
+//             + "вы бы взяли приступом согласие прусского короля. "
+//             + "Вы так красноречивы. Выдадите мне чаю?";
 
-string Replase(string text, char oldValue,char newValue)
+// string Replase(string text, char oldValue,char newValue)
+// {
+//     string result = string.Empty;
+
+//     int lenght = text.Length;
+//     for (int i = 0; i < lenght; i++)
+//     {
+//         if(text[i]==oldValue) result = result +$"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+// string newText = Replase(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replase(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replase(newText, 'С', 'с');
+// Console.WriteLine(newText);
+
+
+// int[] arr = { 1, 5, 4, 3, 2, 8, 7, 6, 1, 6 };
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j;
+//         }
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
+
+// PrintArray(arr);
+// SelectionSort(arr);
+
+// PrintArray(arr);
+
+int[] arr = { 1, 5, 4, 3, 2, 8, 7, 6, 1, 6 };
+
+void PrintArray(int[] array)
 {
-    string result = string.Empty;
+    int count = array.Length;
 
-    int lenght = text.Length;
-    for (int i = 0; i < lenght; i++)
+    for (int i = 0; i < count; i++)
     {
-        if(text[i]==oldValue) result = result +$"{newValue}";
-        else result = result + $"{text[i]}";
+        Console.Write($"{array[i]} ");
     }
-
-    return result;
+    Console.WriteLine();
 }
-string newText = Replase(text, ' ', '|');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replase(newText, 'к', 'К');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replase(newText, 'С', 'с');
-Console.WriteLine(newText);
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
